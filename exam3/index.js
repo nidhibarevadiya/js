b810adec9f7bf367307be6059ab698883e36b12c
nidhibarevadiya <nidhi456barevadiy@gmail.com>
2025-02-24 18:14:32 +0530

    js

---
+++ exam3/index.js
@@ -0,0 +1,93 @@
+let arr=[];
+
+const exl=(index)=>{
+    if(employee1.expriance < 3&& employee1.expriance>0){
+            `jonior`;        }
+        elseif(employee1.expriance<=3&& employee1.expriance>=5)
+        {
+            `mid`;      }  
+
+        else {
+            `senior`;   
+}
+}
+const handledelet = (index)=> {
+    arr.splice(index,1);
+    uiemp();
+    };
+function alldelet(){
+    arr=[];                           
+    uiemp();
+}
+const uiemp =()=>{
+    document.getElementById("tablebody").innerHTML="";
+    arr.map((employee1,i)=>{
+        let td1=document.createElement("td");
+        td1.innerHTML=employee1.name;
+
+        let td2=document.createElement("td");
+        td2.innerHTML=employee1.number;
+
+        let td3=document.createElement("td");
+        td3.innerHTML=employee1.email;
+
+        let td4=document.createElement("td");
+        td4.innerHTML=employee1.jobroll;
+
+        let td5=document.createElement("td");
+        td5.innerHTML=employee1.expriance;
+
+        let td9=document.createElement("td");
+        td9.addEventListener("submit",() => exl(i));
+        
+
+        let td6=document.createElement("td");
+        td6.innerHTML=employee1.salary;
+
+        let img=document.createElement("img");
+        img.src=employee1.img1;
+        let td7=document.createElement("td");
+        td7.append(img);
+
+        let but=document.createElement("button");
+        but.addEventListener("click", ()=>handledelet(i))
+           let td8=document.createElement("td");
+           td8.append(but);
+   
+           let tr=document.createElement("tr");
+           tr.append(td1,td2,td3,td4,td5,td6,td7,td8,td9);
+           document.getElementById("tablebody").append(tr);
+   
+
+});
+}
+
+const submite = (e) =>{
+    e.preventDefault();
+    let name =document.getElementById("name").value;
+    let number= document.getElementById("number").value;
+    let email= document.getElementById("email").value;
+    let jobroll= document.getElementById("jobroll").value;
+    let expriance= document.getElementById("enumber").value;
+    let salary= document.getElementById("salary").value;
+    let image= document.getElementById("img1").value;
+
+    let emp1={
+        name:name,
+        number:number,
+        email:email,
+        jobroll:jobroll,
+        expriance:expriance,
+        salary:salary,
+        image:image
+    };
+
+   
+    arr.push(emp1);
+    // console.log(arr);
+    uiemp();
+    
+
+};
+
+document.getElementById("emp").addEventListener("submit", submite);
---
+++ method/method.js
@@ -0,0 +1,9 @@
+let a=10;
+let b =20;
+console.log(Math.max(a,b));
+console.log(Math.min(a,b));
+console.log(Math.floor(5.8));
+console.log(Math.ceil(5.8));
+console.log(Math.round(5.1))
+console.log(Math.round(5.8))
+
