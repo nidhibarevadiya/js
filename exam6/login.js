cba2409b1c55929d7608b628f4a6d81708c327ae
nidhibarevadiya <nidhi456barevadiy@gmail.com>
2025-04-01 11:10:01 +0530

    js

---
+++ exam6/login.js
@@ -0,0 +1,43 @@
+let sing = JSON.parse(localStorage.getItem("sings")) || [];  
+
+const IsExists = (email) => {
+    let isUser = sing.find((singuser) => singuser.email === email)
+    if (isUser) {
+        return { found: true, singuser: isUser };
+    }
+    else {
+        return { found: false, singuser: isUser };
+    }
+
+};
+
+
+
+const handleSubmit = (e) => {
+  e.preventDefault(); 
+  let login = {
+    email: document.getElementById("email").value,
+    password: document.getElementById("password").value,
+  };
+
+
+    let userData = IsExists(login.email);
+    if (userData.found) {
+        if (userData.singuser.password == login.password)
+        {
+            alert("login success");
+        }
+        else {
+            alert("password does not match")
+        }
+    }
+    else {
+        alert("invalid email");
+    }
+
+  
+};
+
+debugger
+
+document.getElementById("loginpage").addEventListener("submit", handleSubmit);
