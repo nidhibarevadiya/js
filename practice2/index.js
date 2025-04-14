63a2141011bd8977c3decdb76478776595537102
nidhibarevadiya <nidhi456barevadiy@gmail.com>
2025-04-14 15:48:43 +0530

    js

--- practice2/index.js
+++ practice2/index.js
@@ -1,50 +1,4 @@
-//  const createProduct = async (product) => {
-//     let req = await fetch('http://localhost:3000/products', {
-//         method: 'POST',
-//         headers: { 'Content-Type': 'application/json' },
-//         body: JSON.stringify(product)
-//     });
-//     let res = await req.json();
-//     console.log(res);
-//     uimaker(res); 
-// }
 
-// const handelsubmit = (e) => {
-//     e.preventDefault();
-//     let product = {
-//         title: document.getElementById('title').value,
-//         price: document.getElementById('price').value,
-//         img: document.getElementById('img').value
-//     }
-
-//     createProduct(product);
-//     // uimaker(product); 
-
-// }
-
-
-// const uimaker = (product) => {
-
-//     let title = document.createElement('h3');
-//     title.innerText = product.title;
-
-//     let price = document.createElement('h5');
-//     price.innerText = product.price;
-
-//     let img = document.createElement('img');
-//     img.src = product.img;
-
-//     let div = document.createElement('div');
-//     div.append(img, title, price);
-
-//     document.getElementById("productList").append(div);
-//     }
-
-
-
-
-
-// document.getElementById("productdata").addEventListener("submit", handelsubmit)
 
 
 const createProduct = async (product) => {
@@ -55,7 +9,7 @@ const createProduct = async (product) => {
     });
     let res = await req.json();
     console.log(res);
-    uimaker(res); 
+    uimaker(res);
 }
 
 
@@ -65,7 +19,7 @@ const getData = async () => {
         let req = await fetch("http://localhost:3000/products");
         let res = await req.json();
 
-       
+
         res.forEach(product => {
             uimaker(product);
         });
@@ -74,9 +28,9 @@ const getData = async () => {
     }
 };
 
-  
-  getData();
-    
+
+getData();
+
 
 const handleSubmit = (e) => {
     e.preventDefault();
